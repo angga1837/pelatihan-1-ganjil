e6f05336e0996b8b73cf33f2b5181323438dfe99
angga1837 <jundierlangga@gmail.com>
2025-11-02 22:57:50 +0700

    penerapan async, usestate, useffect

--- app/page.js
+++ app/page.js
@@ -1,103 +1,141 @@
-import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+/*export default function Home() {
+  return <div className="bg-[blue] hover:bg-[white] ">
+    Hello world
+    </div>
+    
+}*/
+/*function Display({NIM ,children, className}){
+  return <div className={`text-gray-700 text-[70px] ${className}`} >NIM: {NIM} {children} </div>
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+export default function page() {
+  const nimnim = [9193, 9186, 9097, 9193];
+  return(
+    <div className="bg-amber-500"> 
+      {
+        nimnim.map(function(value, index){
+          return <Display key={JSON.stringify(value, index)} NIM={value}>
+            {index}
+          </Display>
+        })
+      }
     </div>
   );
-}
+}*/
+
+
+/*export default function page(){
+  return <div className="bg-[red]">
+    JANGGA 
+    <Display NIM="25/561570/TK/63456"/> 
+    <Display NIM="25/578900/KH/77689"/>
+    <Display NIM="25/577899/TK/10212">
+      <Display NIM="NONe" className={"bg-amber-100"} />
+    </Display>
+    </div>
+}*/
+
+//usestate
+/*"use client";
+import { useState } from "react";
+
+export default function page(){
+  const [Data, SetData] = useState(2);
+
+  const handleClick = () => {
+    SetData(Data+1);
+  };
+  return (
+    <div className="w-full h-screen flex justify-center align-middle items-center flex-col">
+      <button onClick={handleClick} className="bg-[blue]">
+        klik aku {" "}
+      </button>
+      <div>data adalah {Data}</div> 
+    </div>
+  );
+}*/
+
+//useeffect
+/*"use client";
+import { useEffect, useState } from "react";
+
+export default function page(){
+  const [Data, SetData] = useState("LOADING.....");
+  useEffect(() => {
+    console.log("UseEffect lagi dimakai");
+    SetData("Data masuk");
+  }, []);//kalau kosong, fungsi cuman sekali
+  return <div>Data berisi {Data}</div>;
+}*/
+
+//useRef
+/*"use client";
+import { useEffect, useRef } from "react";
+export default function page(){
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    console.log(divRef);
+  }, []);
+  return(
+    <div ref={divRef} className="w-full">
+      EAA {JSON.stringify(divRef.current)}
+    </div>
+  );
+}*/
+
+//memanggil API dengan Axios
+/*"use client";
+import axios from "axios";
+import { useEffect } from "react";
+
+export default function Page(){
+  useEffect(() => {
+    const init = async () => {
+      console.log("1. Memulai request API....."); //mulai reques API, tapi jangan ditunggu
+      const dataPromise = axios.get("https://be-express-pi.vercel.app/"); //memngembalikan sebauah janji kalau data akan ada nanti
+      console.log("2. ini pasti lebih cepat dari hasil API", dataPromise); // langsung jalan karena tidak ada await pada kode sebelumnya
+      const respone = await dataPromise; //menunggu janji
+      console.log("3 Request API selesai.");
+      console.log(respone);
+    };
+    init();
+  },[]);
+  return <></>;
+}*/
+
+//tes
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function page(){
+  const [Data, SetData] = useState(0);
+
+  const handleClick = () => {
+    SetData(Data+1);
+  };
+  useEffect(() => {
+    const init = async () => {
+      console.log("1. Memulai request API....."); //mulai reques API, tapi jangan ditunggu
+      const dataPromise = axios.get("https://be-express-pi.vercel.app/"); //memngembalikan sebauah janji kalau data akan ada nanti
+      console.log("2. ini pasti lebih cepat dari hasil API", dataPromise); // langsung jalan karena tidak ada await pada kode sebelumnya
+      const respone = await dataPromise; //menunggu janji
+      console.log("3 Request API selesai.");
+      console.log(respone);
+    };
+    init();
+  },[]);
+
+  return (
+    <div className="w-full h-screen flex justify-center align-middle items-center flex-col">
+      <button onClick={handleClick} className="bg-[blue]">
+        klik aku {" "}
+      </button>
+      <div>data adalah {Data}</div> 
+    </div>
+  );
+};
+
+
